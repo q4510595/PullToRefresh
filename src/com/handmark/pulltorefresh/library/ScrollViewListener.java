4cfd03356ff8dc3e69c130dc5a64c57016479227
bky <707494125@qq.com>
2016-11-08 11:09:25 +0800

    提交下拉刷新

---
+++ src/com/handmark/pulltorefresh/library/ScrollViewListener.java
@@ -0,0 +1,10 @@
+package com.handmark.pulltorefresh.library;
+
+
+
+public interface ScrollViewListener {
+
+    void onScrollChanged(PullToRefreshScrollView scrollView, int x, int y, int oldx, int oldy);
+
+}
+
